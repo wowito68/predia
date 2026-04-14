56f8b4cac20ea0e85010dce363520cdc8e48dc61
wowito68 <memohalo12@hotmail.com>
2026-04-14 15:23:16 -0600

    -

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,6 +1,6 @@
 describe('Basic Application Test', () => {
   it('should verify that true is true to ensure CI pipeline runs tests properly', () => {
-    expect(true).toBe(false); // ESTE ERROR ROMPE EL CI INTENCIONALMENTE
+    expect(true).toBe(true); // ESTE ERROR ROMPE EL CI INTENCIONALMENTE
   });
   
   it('verifies CI integration works correctly', () => {
